5db7718b5c3b9beecd59385947ae767f51c2b689
Aniela M Wolkonowski <anielamw@gmail.com>
2016-08-09 14:24:46 -0400

    add backstory

--- code.rb
+++ code.rb
@@ -53,6 +53,9 @@ class Brawler < SuperHero
 end
 
 class Detective < SuperHero
+  def backstory
+    "Private Luke Bayne, while walking his late night beat, accidentally ate toxic-frosted yellow-cake donuts and awoke the next morning with over heightened senses that make him acutely hyperaware of his surroundings. He has managed to channel these skills into his chosen calling, but having gone from being an inept foot-patroller to the keenest of investigators, he is now suspected of being corrupt and under and EA investigation that has no proof, only speculation. Taking his new found skills underground, he has teamed up with the Launcher League to do good and fight crime all while remaining mild-mannered Private Luke Bayne amongst his colleagues."
+  end
 end
 
 class Demigod < SuperHero
