5167b88516ae33ae6448395c27fb4db36dcb1f6a
Aniela M Wolkonowski <anielamw@gmail.com>
2016-08-09 18:29:21 -0400

    added the jack of all trade's story

--- code.rb
+++ code.rb
@@ -120,6 +120,29 @@ class Demigod < SuperHero
 end
 
 class JackOfAllTrades < SuperHero
+  def species
+    "Alien"
+  end
+
+  def home
+    "Venus"
+  end
+
+  def powers
+    "Invisibility, shape-shifting, mind reading, telekinesis and pseudo-tactile euphoric transference."
+  end
+
+  def weakness
+    "Easily susceptible to romance/lust and the common cold."
+  end
+
+  def backstory
+    "On a routine scouting mission through the hyper-way to maintain peace and prosperity in the solar cluster, Brig 8’s ship suffered and ID 10t error and crashed just outside of Barstow. The Launch League fearing an invasion rushed to the scene and found her injured, but conscious and very wonderful. They nursed her to health and aided her assimilation into Earth culture. While repairs on her ship process slowly Brigette Barstow uses her alien abilities and tech to maintain peace and prosperity on Earth until she can return to her home on Venus."
+  end
+
+  def psychic?
+    true
+  end
 end
 
 class WaterBased < SuperHero
